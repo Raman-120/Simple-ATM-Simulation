921d78ed682040452c40f7cc00676922687c4ab8
Raman-120 <ramanchaulagain059@gmail.com>
2026-07-05 17:19:25 +0545

    set up the Account class

--- ATM Simulation/src/Account.java
+++ ATM Simulation/src/Account.java
@@ -1,2 +1,22 @@
-public class Account {
+import java.io.File;
+import java.io.Serializable;
+import java.util.Scanner;
+
+public class Account implements Serializable{
+
+    private Scanner scanner;
+    private File file = new File("Users.ser");
+
+    private String cardNumber;
+    private String pin;
+
+    private String getCardNumber(){
+        return this.cardNumber;
+    }
+
+    private String getPin(){
+        return this.pin;
+    }
+
+
 }
